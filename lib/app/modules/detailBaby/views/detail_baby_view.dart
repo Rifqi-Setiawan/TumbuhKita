94b0482c9247801c09ffc889ec12a030da3f994d
Rifqi Setiawan <rifqis232@gmail.com>
2025-05-28 08:08:43 +0700

    menyelesaikan detail baby

--- lib/app/modules/detailBaby/views/detail_baby_view.dart
+++ lib/app/modules/detailBaby/views/detail_baby_view.dart
@@ -35,42 +35,38 @@ class DetailBabyView extends GetView<DetailBabyController> {
             style: AppTextStyles.body1Bold.copyWith(color: Color(0xff2C0B61)),
           ),
           SizedBox(height: 108.h),
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.topCenter,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 21.w),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50.sp),
-                    topRight: Radius.circular(50.sp),
+          Expanded(
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 21.w),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50.sp),
+                      topRight: Radius.circular(50.sp),
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(height: 79.h),
-                    CustomBabyDetailCard(),
-                    SizedBox(height: 20.h),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Grafik Pertumbuhan",
-                        style: AppTextStyles.heading7Bold.copyWith(
-                          color: Color(0xff2C0B61),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 79.h),
+                      CustomBabyDetailCard(),
+                      SizedBox(height: 20.h),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Grafik Pertumbuhan",
+                          style: AppTextStyles.heading7Bold.copyWith(
+                            color: Color(0xff2C0B61),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 5.h),
-                    CategoryChartDetailbaby(),
-                    SizedBox(height: 15.h),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.sp),
-                        
-                      ),
-                      child: Column(
+                      SizedBox(height: 5.h),
+                      CategoryChartDetailbaby(),
+                      SizedBox(height: 15.h),
+                      Column(
                         children: [
                           DetailBabyChartComponents(
                             spots: controller.weightDataPoints,
@@ -79,27 +75,119 @@ class DetailBabyView extends GetView<DetailBabyController> {
                             minY: 0,
                             maxY: 100,
                           ),
-                          SizedBox(height: 8.h),
+                          SizedBox(height: 10.h),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Image.asset("assets/images/IkonChart.png"),
+                              Text(
+                                "Tinggi Badan",
+                                style: AppTextStyles.caption1Regular.copyWith(
+                                  color: Colors.black.withOpacity(0.7),
+                                ),
+                              ),
                             ],
+                          ),
+                          SizedBox(height: 5.h),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Tinggi Badan",
+                                  style: AppTextStyles.body3Medium,
+                                ),
+                                Container(
+                                  width: 220.w,
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 15.w,
+                                    vertical: 5.h,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black.withOpacity(0.1),
+                                      width: 1.sp,
+                                    ),
+                                    borderRadius: BorderRadius.circular(50.sp),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("50 cm"),
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                          horizontal: 14.w,
+                                          vertical: 5.h,
+                                        ),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15.r),
+                                          color: AppColors.error70,
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            "Pendek",
+                                            style: AppTextStyles.caption2Regular
+                                                .copyWith(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Pertumbuhan",
+                                  style: AppTextStyles.body3Medium,
+                                ),
+                                Container(
+                                  width: 220.w,
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 15.w,
+                                    vertical: 5.h,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.warning30,
+                                    border: Border.all(
+                                      color: Colors.black.withOpacity(0.1),
+                                      width: 1.sp,
+                                    ),
+                                    borderRadius: BorderRadius.circular(50.sp),
+                                  ),
+                                  child: Center(
+                                    child: Text("Kurang Baik",
+                                     style: AppTextStyles.caption1Semibold,),
+                                  )
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: -80.sp,
-                child: Center(
-                  child: CustomProfileBaby(
-                    profileImagePath: "assets/images/profileBayi.png",
+                    ],
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  top: -80.sp,
+                  child: Center(
+                    child: CustomProfileBaby(
+                      profileImagePath: "assets/images/profileBayi.png",
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
